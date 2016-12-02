78ca5d8ba7a625a4fe9e773257fc5c9fe9e27d19
Garrett Cullen <garrettwcullen@gmail.com>
2016-12-02 15:26:38 -0800

    push

--- wp-content/themes/gustafsonandgluek/page-main.php
+++ wp-content/themes/gustafsonandgluek/page-main.php
@@ -57,7 +57,25 @@ get_header(); ?>
 
 <section class="about">
 	
-	<span class="large_header"></span><!-- large_header -->
+	<span class="large_header">Minneapolis Class Action Attorneys</span><!-- large_header -->
+	
+	<div class="about_content">
+		
+		<span class="about_excerpt">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas et ornare lectus, non pharetra nisl. Phasellus quis nunc tortor. Pellentesque in eros a lacus egestas malesuada. Integer nec tempus mi. Integer cursus tellus mi, et lobortis mi condimentum porttitor. Proin hendrerit ligula lorem, eget finibus quam efficitur vitae. Duis et metus at nulla suscipit pretium. Nulla in orci feugiat, hendrerit nunc at, luctus nisi. Fusce porta quam magna.</span><!-- about_excerpt -->
+		
+		<span class="about_title">Massa rutrum justo commondo</span><!-- about_title -->
+		
+		<p>Nulla sagittis eleifend tellus, et ultrices massa rutrum id. Maecenas justo augue, commodo nec est ac, tincidunt egestas diam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec placerat sed metus ut mollis. Vestibulum vel semper justo. Sed leo tortor, faucibus eget congue id, sollicitudin varius dui. Praesent blandit elit vehicula tortor suscipit cursus. Pellentesque pellentesque bibendum aliquam. Suspendisse fermentum sagittis mauris, sit amet aliquam quam gravida et. Vivamus non mollis enim.</p>
+		
+		<p>Cras quis mattis ex. Quisque quam arcu, lacinia at metus nec, ornare luctus dolor. Integer facilisis nisi odio, vel mattis ex tristique id. Mauris risus urna, imperdiet sit amet fringilla imperdiet, pharetra id orci. Sed nec orci sed sapien ornare scelerisque nec sit amet mi. Mauris ultricies dolor egestas, posuere ipsum in, hendrerit nisi. Nulla malesuada aliquet tellus, et commodo ligula dapibus vel. Ut turpis est, venenatis id enim tempor, fermentum malesuada ligula. Ut nec lacinia diam. Nulla imperdiet, enim at luctus. eget nibh sit amet nunc tempus vestibulum. Cras convallis rhoncus leo at pellentesque. Vivamus eget consequat dolor. Maecenas eget nisl hendrerit dolor imperdiet interdum. Phasellus nisi enim, maximus eu mattis nec, posuere et lacus. Praesent blandit elit vehicula tortor suscipit cursus. Pellentesque pellentesque bibendum aliquam. Suspendisse fermentum sagittis mauris, sit amet aliquam quam gravida et. Vivamus non mollis enim.</p>
+		
+	</div><!-- about_content -->
+	
+	<div class="about_pic">
+		
+		<img src="<?php bloginfo('template_directory');?>/images/image_01_ipad.jpg"/>
+		
+	</div><!-- about_pic  -->
 	
 	
 </section><!-- about -->
