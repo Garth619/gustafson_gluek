9ce82fa798937b39f0541f74cec3ddd4c29f8463
Garrett Cullen <garrettwcullen@gmail.com>
2016-12-05 15:08:26 -0800

    push

--- wp-content/themes/gustafsonandgluek/footer.php
+++ wp-content/themes/gustafsonandgluek/footer.php
@@ -1,4 +1,49 @@
 
+<footer>
+	
+	<div class="address_box">
+		
+		<img class="pin" src="<?php bloginfo('template_directory');?>/images/footer_map.png"/>
+		
+		<span class="address">120 South 6th Street<br/>Suite 2600<br/>Minneapolis, MN 55402</span>
+		
+		<a class="tel" href="tel:612.333.8844">612.333.8844</a>
+		
+		<a class="green_button" href="">Map</a>
+		
+	</div><!-- address_box -->
+
+	<div class="social_media">
+		
+		<span class="follow_us">Follow Us</span><!-- follow_us -->
+		
+		<div class="social_media_wrapper">
+		
+		<a href="" target="_blank"><img class="sm_icon" src="<?php bloginfo('template_directory');?>/images/footer_fb_green.svg"/></a>
+		<a href="" target="_blank"><img class="sm_icon" src="<?php bloginfo('template_directory');?>/images/footer_google_green.svg"/></a>
+		<a href="" target="_blank"><img class="sm_icon" src="<?php bloginfo('template_directory');?>/images/footer_linkedin_green.svg"/></a>
+		
+		</div><!-- social_media_wrapper -->
+		
+	</div><!-- social_media -->
+	
+	<div class="copyright">
+		
+		<span>Copyright 2016 Gustafson Gluek PLLC. ALL RIGHTS RESERVED.</span>
+		
+		<a class="disclaimer" href="">Disclaimer</a>
+		
+	</div><!-- copyright -->
+	
+	<div class="ilawyer">
+		
+		<a href="//ilawyermarketing.com" target="_blank">
+			<img src="<?php bloginfo('template_directory');?>/images/ilawyer.svg"/>
+		</a>
+		
+	</div><!-- ilawyer -->
+
+</footer>
 	
 <?php wp_footer(); ?>
 
