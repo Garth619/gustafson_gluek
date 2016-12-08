058765cbc8f626edf2a749a122b5301ec5ad89b3
Garrett Cullen <garrettwcullen@gmail.com>
2016-12-07 16:10:21 -0800

    push

--- wp-content/themes/gustafsonandgluek/page-main.php
+++ wp-content/themes/gustafsonandgluek/page-main.php
@@ -61,6 +61,42 @@ get_header(); ?>
 	</div><!-- single_slide_wrapper -->
 	
 	
+	<div class="single_slide_wrapper">
+	
+	<div class="slide">
+		
+		<img class="svg_icon" src="<?php bloginfo('template_directory');?>/images/sp-01.svg"/>
+		
+		<span class="large_header">Over 70 years of experience</span><!-- large_header -->
+		
+		<div class="gold_line"></div><!-- Gold Line -->
+		
+		<span class="sub_content">Nulla sagittis eleifend tellus, et ultrices massa rutrum id. Maecenas justo augue </span><!-- sub_content -->
+		
+		
+	</div><!-- slide -->
+	
+	</div><!-- single_slide_wrapper -->
+	
+	
+	<div class="single_slide_wrapper">
+	
+	<div class="slide">
+		
+		<img class="svg_icon" src="<?php bloginfo('template_directory');?>/images/sp-01.svg"/>
+		
+		<span class="large_header">Over 70 years of experience</span><!-- large_header -->
+		
+		<div class="gold_line"></div><!-- Gold Line -->
+		
+		<span class="sub_content">Nulla sagittis eleifend tellus, et ultrices massa rutrum id. Maecenas justo augue </span><!-- sub_content -->
+		
+		
+	</div><!-- slide -->
+	
+	</div><!-- single_slide_wrapper -->
+	
+	
 	</div><!-- slide_wrapper -->
 	
 	<div style="clear:both;"></div><!-- clear hack -->
@@ -72,27 +108,37 @@ get_header(); ?>
 	
 	<span class="large_header">Minneapolis Class Action Attorneys</span><!-- large_header -->
 	
-	<div class="about_content">
-		
-		<span class="about_excerpt">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas et ornare lectus, non pharetra nisl. Phasellus quis nunc tortor. Pellentesque in eros a lacus egestas malesuada. Integer nec tempus mi. Integer cursus tellus mi, et lobortis mi condimentum porttitor. Proin hendrerit ligula lorem, eget finibus quam efficitur vitae. Duis et metus at nulla suscipit pretium. Nulla in orci feugiat, hendrerit nunc at, luctus nisi. Fusce porta quam magna.</span><!-- about_excerpt -->
-		
-		<span class="about_title">Massa rutrum justo commondo</span><!-- about_title -->
-		
-		<p>Nulla sagittis eleifend tellus, et ultrices massa rutrum id. Maecenas justo augue, commodo nec est ac, tincidunt egestas diam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec placerat sed metus ut mollis. Vestibulum vel semper justo. Sed leo tortor, faucibus eget congue id, sollicitudin varius dui. Praesent blandit elit vehicula tortor suscipit cursus. Pellentesque pellentesque bibendum aliquam. Suspendisse fermentum sagittis mauris, sit amet aliquam quam gravida et. Vivamus non mollis enim.</p>
-		
-		<p>Cras quis mattis ex. Quisque quam arcu, lacinia at metus nec, ornare luctus dolor. Integer facilisis nisi odio, vel mattis ex tristique id. Mauris risus urna, imperdiet sit amet fringilla imperdiet, pharetra id orci. Sed nec orci sed sapien ornare scelerisque nec sit amet mi. Mauris ultricies dolor egestas, posuere ipsum in, hendrerit nisi. Nulla malesuada aliquet tellus, et commodo ligula dapibus vel. Ut turpis est, venenatis id enim tempor, fermentum malesuada ligula. Ut nec lacinia diam. Nulla imperdiet, enim at luctus. eget nibh sit amet nunc tempus vestibulum. Cras convallis rhoncus leo at pellentesque. Vivamus eget consequat dolor. Maecenas eget nisl hendrerit dolor imperdiet interdum. Phasellus nisi enim, maximus eu mattis nec, posuere et lacus. Praesent blandit elit vehicula tortor suscipit cursus. Pellentesque pellentesque bibendum aliquam. Suspendisse fermentum sagittis mauris, sit amet aliquam quam gravida et. Vivamus non mollis enim.</p>
-		
-	</div><!-- about_content -->
 	
-	<div class="about_pic">
+	<div class="about_inner_wrapper">
+	
+		<div class="about_content">
+			
+			<span class="about_excerpt">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas et ornare lectus, non pharetra nisl. Phasellus quis nunc tortor. Pellentesque in eros a lacus egestas malesuada. Integer nec tempus mi. Integer cursus tellus mi, et lobortis mi condimentum porttitor. Proin hendrerit ligula lorem, eget finibus quam efficitur vitae. Duis et metus at nulla suscipit pretium. Nulla in orci feugiat, hendrerit nunc at, luctus nisi. Fusce porta quam magna.</span><!-- about_excerpt -->
+			
+			<span class="about_title">Massa rutrum justo commondo</span><!-- about_title -->
+			
+			<p>Nulla sagittis eleifend tellus, et ultrices massa rutrum id. Maecenas justo augue, commodo nec est ac, tincidunt egestas diam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec placerat sed metus ut mollis. Vestibulum vel semper justo. Sed leo tortor, faucibus eget congue id, sollicitudin varius dui. Praesent blandit elit vehicula tortor suscipit cursus. Pellentesque pellentesque bibendum aliquam. Suspendisse fermentum sagittis mauris, sit amet aliquam quam gravida et. Vivamus non mollis enim.</p>
+			
+			<p>Cras quis mattis ex. Quisque quam arcu, lacinia at metus nec, ornare luctus dolor. Integer facilisis nisi odio, vel mattis ex tristique id. Mauris risus urna, imperdiet sit amet fringilla imperdiet, pharetra id orci. Sed nec orci sed sapien ornare scelerisque nec sit amet mi. Mauris ultricies dolor egestas, posuere ipsum in, hendrerit nisi. Nulla malesuada aliquet tellus, et commodo ligula dapibus vel. Ut turpis est, venenatis id enim tempor, fermentum malesuada ligula. Ut nec lacinia diam. Nulla imperdiet, enim at luctus. eget nibh sit amet nunc tempus vestibulum. Cras convallis rhoncus leo at pellentesque. Vivamus eget consequat dolor. Maecenas eget nisl hendrerit dolor imperdiet interdum. Phasellus nisi enim, maximus eu mattis nec, posuere et lacus. Praesent blandit elit vehicula tortor suscipit cursus. Pellentesque pellentesque bibendum aliquam. Suspendisse fermentum sagittis mauris, sit amet aliquam quam gravida et. Vivamus non mollis enim.</p>
+			
+		</div><!-- about_content -->
 		
-		<img class="group_pic mobile" src="<?php bloginfo('template_directory');?>/images/image_01_mobile.jpg"/>
-		<img class="group_pic ipad" src="<?php bloginfo('template_directory');?>/images/image_01_ipad.jpg"/>
-		<img class="tritwo mobile" src="<?php bloginfo('template_directory');?>/images/element_triangle2.svg"/>
-		<img class="tritwo ipad" src="<?php bloginfo('template_directory');?>/images/element_triangle.svg"/>
+		<div class="about_pic">
+			
+			<img class="group_pic mobile" src="<?php bloginfo('template_directory');?>/images/image_01_mobile.jpg"/>
+			<img class="group_pic ipad" src="<?php bloginfo('template_directory');?>/images/image_01_ipad.jpg"/>
+			<img class="group_pic desktop" src="<?php bloginfo('template_directory');?>/images/imge_01.jpg"/>
+			
+			
+			<img class="tritwo mobile" src="<?php bloginfo('template_directory');?>/images/element_triangle2.svg"/>
+			<img class="tritwo ipad" src="<?php bloginfo('template_directory');?>/images/element_triangle.svg"/>
+			
+			
+		</div><!-- about_pic  -->
 		
-		
-	</div><!-- about_pic  -->
+	
+	
+	</div><!-- about_inner_wrapper -->
 	
 	
 </section><!-- about -->
@@ -111,16 +157,28 @@ get_header(); ?>
 
 <section id="case_results">
 	
-	<span class="large_header">Our Case Summaries</span><!-- large_header -->
 	
-	<div class="case_results_content_wrapper">
+	<div class="desktop_case_results_content_wrapper">
 	
-		<p>Our law firm  has a national complex litigation practice focused on antitrust and consumer protection class actions.</p>
+		<span class="large_header">Our Case Summaries</span><!-- large_header -->
 	
-		<a class="gold_button mobile" href="">Case Summaries</a><!-- gold_button -->
-		<a class="green_button summaries_button" href="">View More Summaries</a><!-- green_button -->
+		<div class="case_results_content_wrapper">
+	
+			<p>Our law firm  has a national complex litigation practice focused on antitrust and consumer protection class actions.</p>
+	
+			<a class="gold_button mobile" href="">Case Summaries</a><!-- gold_button -->
+			<a class="green_button summaries_button" href="">View More Summaries</a><!-- green_button -->
 		
-	</div><!-- case_results_content_wrapper -->
+		</div><!-- case_results_content_wrapper -->
+	
+	</div><!-- desktop_case_results_content_wrapper -->
+	
+	
+	
+	
+	
+	
+	
 	
 	<div class="case_results_slide_wrapper">
 		
@@ -145,16 +203,55 @@ get_header(); ?>
 					<span>At issue in the cases were public statements as well as material omissions St. Paul Travelers made that negatively impacted the stock prices of the Company. On behalf of New Mexico State Funds, Gustafson Gluek worked to litigate the two separate class actions against St. Paul Travelers, resulting in multi-million dollar settlements.</span>
 					
 					
-				</div><!-- case_results_content_wrapper -->
+				</div><!-- case_results_content -->
 				
 			</div><!-- slide -->
 			
 		</div><!-- slide_wrapper -->
 		
+		
+		<div class="slide_wrapper">
+			
+			<div class="slide">
+				
+				<div class="title_left">
+				
+					<span class="slide_header">St. Paul Travelers</span><!-- large_header -->
+					<span class="slide_sub_header">Securities</span><!-- slide_sub_header -->
+				
+				</div><!-- title_left -->
+				
+				<img class="case_results_icon" src="<?php bloginfo('template_directory');?>/images/results_securities.png"/>
+				
+				<div class="case_results_content">
+					
+					
+					<div class="gold_line"></div><!-- gold_line -->
+					
+					<span>At issue in the cases were public statements as well as material omissions St. Paul Travelers made that negatively impacted the stock prices of the Company. On behalf of New Mexico State Funds, Gustafson Gluek worked to litigate the two separate class actions against St. Paul Travelers, resulting in multi-million dollar settlements.</span>
+					
+					
+				</div><!-- case_results_content -->
+				
+			</div><!-- slide -->
+			
+		</div><!-- slide_wrapper -->
+		
+		
+		
+		
+		
+		
 	</div><!-- case_results_slide_wrapper -->
 	
 	
+
 </section><!-- case_results -->
+
+
+
+
+
 
 <section id="awards">
 	
@@ -182,8 +279,8 @@ get_header(); ?>
 		
 		<div class="ex_pic_wrapper">
 		
-			<img class="ex_pic mobile"src="<?php bloginfo('template_directory');?>/images/image_02_mobile.jpg"/>
-			<img class="ex_pic ipad"src="<?php bloginfo('template_directory');?>/images/image_02_ipad.jpg"/>
+			<img class="ex_pic mobile" src="<?php bloginfo('template_directory');?>/images/image_02_mobile.jpg"/>
+			<img class="ex_pic ipad" src="<?php bloginfo('template_directory');?>/images/image_02_ipad.jpg"/>
 			<img class="tritwo mobile" src="<?php bloginfo('template_directory');?>/images/element_triangle2.svg"/>
 		
 		</div><!-- ex_pic  -->
