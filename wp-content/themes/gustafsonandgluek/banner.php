8b9e4c273fd0706cefa365d53a2cd80a66c361c3
Garrett Cullen <garrettwcullen@gmail.com>
2016-12-09 16:06:42 -0800

    push

--- wp-content/themes/gustafsonandgluek/banner.php
+++ wp-content/themes/gustafsonandgluek/banner.php
@@ -1,5 +1,19 @@
 <div class="inner_banner">
 	
+	<div class="inner_hero_wrapper">
+	
+		<div class="hero_banner_content">
+	
+			<span class="sub_header">Dedicated to the protection of </span><!-- sub_header -->
+			<span class="large_header">Consumer Rights and Fair Competition</span><!-- large_header -->
+		
+			<span class="description">National Complex litigation practice</span><!-- description -->
+		
+			<a class="green_button request_button" href="">request your free consultation</a>
+	
+		</div><!-- hero_banner_content -->
+	
+	</div><!-- inner_hero_wrapper -->
 	
 	
 </div><!-- inner_banner -->
