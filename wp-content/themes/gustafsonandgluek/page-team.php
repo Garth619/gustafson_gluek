4646c59a86b4d8669701c7b31f90f6017bdbe03c
Garrett Cullen <garrettwcullen@gmail.com>
2016-12-12 13:54:07 -0800

    push

---
+++ wp-content/themes/gustafsonandgluek/page-team.php
@@ -0,0 +1,198 @@
+<?php
+/**
+ * Template Name: Team
+ *
+
+ */
+
+get_header(); ?>
+
+
+
+<?php include('banner.php');?>
+
+
+<div class="no_sidebar_wrapper">
+			
+			
+			<div class="header_wrapper">
+				
+				<span>Attorneys</span>
+				
+			</div><!-- header_wrapper -->
+			
+			
+			<div class="meet_the_team_wrapper">
+				
+				
+				<a class="team_link" href="">
+					
+					<div class="single_profile_wrapper">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/team_gustafson.jpg"/>
+						
+						<div class="profile_caption">
+							
+							<span class="caption_title">Daniel E. Gustafson</span><!-- caption_title -->
+							
+							<span class="view_profile">View Profile</span><!-- view_profile -->
+							
+						</div><!-- profile_caption -->
+						
+					</div><!-- single_profile_wrapper -->
+					
+				</a><!-- team_link -->
+				
+				<a class="team_link" href="">
+					
+					<div class="single_profile_wrapper">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/team_gustafson.jpg"/>
+						
+						<div class="profile_caption">
+							
+							<span class="caption_title">Daniel E. Gustafson</span><!-- caption_title -->
+							
+							<span class="view_profile">View Profile</span><!-- view_profile -->
+							
+						</div><!-- profile_caption -->
+						
+					</div><!-- single_profile_wrapper -->
+					
+				</a><!-- team_link -->
+				
+				<a class="team_link" href="">
+					
+					<div class="single_profile_wrapper">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/team_gustafson.jpg"/>
+						
+						<div class="profile_caption">
+							
+							<span class="caption_title">Daniel E. Gustafson</span><!-- caption_title -->
+							
+							<span class="view_profile">View Profile</span><!-- view_profile -->
+							
+						</div><!-- profile_caption -->
+						
+					</div><!-- single_profile_wrapper -->
+					
+				</a><!-- team_link -->
+				
+				<a class="team_link" href="">
+					
+					<div class="single_profile_wrapper">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/team_gustafson.jpg"/>
+						
+						<div class="profile_caption">
+							
+							<span class="caption_title">Daniel E. Gustafson</span><!-- caption_title -->
+							
+							<span class="view_profile">View Profile</span><!-- view_profile -->
+							
+						</div><!-- profile_caption -->
+						
+					</div><!-- single_profile_wrapper -->
+					
+				</a><!-- team_link -->
+				
+				<a class="team_link" href="">
+					
+					<div class="single_profile_wrapper">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/team_gustafson.jpg"/>
+						
+						<div class="profile_caption">
+							
+							<span class="caption_title">Daniel E. Gustafson</span><!-- caption_title -->
+							
+							<span class="view_profile">View Profile</span><!-- view_profile -->
+							
+						</div><!-- profile_caption -->
+						
+					</div><!-- single_profile_wrapper -->
+					
+				</a><!-- team_link -->
+				
+				<a class="team_link" href="">
+					
+					<div class="single_profile_wrapper">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/team_gustafson.jpg"/>
+						
+						<div class="profile_caption">
+							
+							<span class="caption_title">Daniel E. Gustafson</span><!-- caption_title -->
+							
+							<span class="view_profile">View Profile</span><!-- view_profile -->
+							
+						</div><!-- profile_caption -->
+						
+					</div><!-- single_profile_wrapper -->
+					
+				</a><!-- team_link -->
+				
+				<a class="team_link" href="">
+					
+					<div class="single_profile_wrapper">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/team_gustafson.jpg"/>
+						
+						<div class="profile_caption">
+							
+							<span class="caption_title">Daniel E. Gustafson</span><!-- caption_title -->
+							
+							<span class="view_profile">View Profile</span><!-- view_profile -->
+							
+						</div><!-- profile_caption -->
+						
+					</div><!-- single_profile_wrapper -->
+					
+				</a><!-- team_link -->
+				
+				<a class="team_link" href="">
+					
+					<div class="single_profile_wrapper">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/team_gustafson.jpg"/>
+						
+						<div class="profile_caption">
+							
+							<span class="caption_title">Daniel E. Gustafson</span><!-- caption_title -->
+							
+							<span class="view_profile">View Profile</span><!-- view_profile -->
+							
+						</div><!-- profile_caption -->
+						
+					</div><!-- single_profile_wrapper -->
+					
+				</a><!-- team_link -->
+				
+				<a class="team_link" href="">
+					
+					<div class="single_profile_wrapper">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/team_gustafson.jpg"/>
+						
+						<div class="profile_caption">
+							
+							<span class="caption_title">Daniel E. Gustafson</span><!-- caption_title -->
+							
+							<span class="view_profile">View Profile</span><!-- view_profile -->
+							
+						</div><!-- profile_caption -->
+						
+					</div><!-- single_profile_wrapper -->
+					
+				</a><!-- team_link -->
+				
+				
+			</div><!-- meet_the_team_wrapper -->					
+			
+		</div><!-- no_sidebar_wrapper -->
+		
+
+
+
+<?php get_footer(); ?>
