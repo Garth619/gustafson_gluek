4522b8b9005f4ed00095e00ddd77af501cb4d926
garrett cullen <info@garrettcullen.com>
2016-12-11 19:42:24 -0800

    push

---
+++ wp-content/themes/gustafsonandgluek/page-practiceareas.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Template Name: Practice Area Directory
+ *
+
+ */
+
+get_header(); ?>
+
+
+
+<?php include('banner.php');?>
+
+
+<div class="no_sidebar_wrapper">
+			
+			
+			<div class="header_wrapper">
+				
+				<span>Practice Areas</span>
+				
+			</div><!-- header_wrapper -->
+			
+			
+			<div class="directory">
+				
+				<ul>
+					<li><a href="">Practice Areas 1</a></li>
+					<li><a href="">Practice Areas 2</a></li>
+					<li><a href="">Practice Areas 3</a></li>
+					<li><a href="">Practice Areas 4</a></li>
+					<li><a href="">Practice Areas 5</a></li>
+					<li><a href="">Practice Areas</a></li>
+					<li><a href="">Practice Areas</a></li>
+					<li><a href="">Practice Areas</a></li>
+					<li><a href="">Practice Areas</a></li>
+					<li><a href="">Practice Areas</a></li>
+					<li><a href="">Practice Areas</a></li>
+					<li><a href="">Practice Areas</a></li>
+					<li><a href="">Practice Areas</a></li>
+					<li><a href="">Practice Areas</a></li>
+					<li><a href="">Practice Areas</a></li>
+					<li><a href="">Practice Areas</a></li>
+					<li><a href="">Practice Areas</a></li>
+					<li><a href="">Practice Areas</a></li>
+				</ul>
+				
+			</div><!-- directory -->
+						
+			
+		</div><!-- no_sidebar_wrapper -->
+		
+
+
+
+<?php get_footer(); ?>
