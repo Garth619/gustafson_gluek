7d646605902ab41972e336e9f5a09447da060f32
Garrett Cullen <garrettwcullen@gmail.com>
2016-12-12 15:39:19 -0800

    push

---
+++ wp-content/themes/gustafsonandgluek/page-bio.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Template Name: Bio 
+ *
+
+ */
+
+get_header(); ?>
+
+
+
+<?php include('banner.php');?>
+
+
+<div class="inner_container">
+	
+	
+	<div class="inner_flex_spacer"></div><!-- inner_flex_spacer -->
+	
+	
+	<div class="desktop_container_wrapper">
+	
+		<?php get_sidebar('bio'); ?>
+		
+		
+		<div class="content_wrapper">
+		
+		
+			<div class="content">
+			
+				<div class="inner_content">
+					
+					<span class="pull_quote">Dan has been designated a “Super Lawyer” by Law & Politics magazine in the fields of business litigation and class actions for the last twelve years.</span><!-- pull_quote -->
+			
+					<?php // get_template_part( 'loop', 'page' ); ?>
+					
+					<p>Dan Gustafson is a founding member of Gustafson Gluek PLLC. He is a magna cum laude graduate of the University of North Dakota (B.S. 1986) and a cum laude graduate of the University of Minnesota Law School (J.D. 1989). He was a member of the Minnesota Law Review from 1987 to 1989, serving as Associate Research Editor in 1988-1989. After law school, Dan served as a law clerk to the Honorable Diana E. Murphy, United States District Judge for the District of Minnesota (1989-91).
+Dan is a member of the Hennepin County, Minnesota State, Federal, and American Bar Associations. He is also currently an adjunct professor at the University of Minnesota Law School teaching “Fundamentals of Pretrial Litigation.”</p>
+
+<p>Dan was the president of the Federal Bar Association, Minnesota Chapter for the 2002-2003 year, and vice-chair of the 2003 Eighth Circuit Judicial Conference. He was involved in helping develop the Federal Bar Association’s Pro Se Project, which coordinates volunteer representation for pro se litigants.
+Minnesota Lawyer magazine named Dan one of its Attorneys of the Year for the second time in 2013. He received the peer-nominated honor for the first time in 2010.</p>
+			
+				</div><!-- inner_content -->
+			
+			</div><!-- content -->
+		
+		
+		</div><!-- content_wrapper -->
+	
+		
+		
+		</div><!-- desktop_container_wrapper -->
+		
+		<div class="inner_flex_spacer"></div><!-- inner_flex_spacer -->
+	
+	</div><!-- inner_container -->
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/gustafsonandgluek/sidebar-bio.php
@@ -0,0 +1,38 @@
+<div class="bio_sidebar">
+
+	
+	<h1 class="inner_header"><?php the_title(); ?></h1><!-- inner_header -->
+	
+	<img src="<?php bloginfo('template_directory');?>/images/profile_gustafson.jpg"/>
+	
+	<div class="bio_content">
+		
+		<span class="bio_sidebar_title">EDuCATION</span><!-- bio_sidebar_title -->
+
+		<ul>
+			<li>University of Minnesota Law School J.D., Cum Laude (1989)</li>
+			<li>University of North Dakota B.S., Magna Cum Laude (1986)</li>
+		</ul>
+		
+		<span class="bio_sidebar_title">COURT ADMISSIONS</span><!-- bio_sidebar_title -->
+
+		<ul>
+			<li>Minnesota State Bar</li>
+			<li>U.S. District Court for the District of Minnesota</li>
+			<li>U.S. Court of Appeals for the First Circuit</li>
+			<li>U.S. Court of Appeals for the Third Circuit</li>
+			<li>U.S. Court of Appeals for the Fifth Circuit</li>
+			<li>U.S. Court of Appeals for the Eighth Circuit</li>
+			<li>U.S. Court of Appeals for the Ninth Circuit</li>
+			<li>U.S. Court of Appeals for the Eleventh Circuit</li>
+			<li>U.S. Supreme Court</li>
+		</ul>
+		
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+	</div><!-- bio_content -->
+	
+	
+</div><!-- bio_sidebar -->
