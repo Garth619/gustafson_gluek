1ad51f30b112adc3e517de3ab644e614b54298d4
Garrett Cullen <garrettwcullen@gmail.com>
2016-12-13 16:00:37 -0800

    push

--- wp-content/themes/gustafsonandgluek/sidebar-caseresults.php
+++ wp-content/themes/gustafsonandgluek/sidebar-caseresults.php
@@ -21,13 +21,30 @@
 				
 				<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 				
+				<hr/>
+				
+				<span class="accordian_title">In re Capacitors Antitrust Litigation (N.D. Cal.)</span><!-- accordian_title -->
+				
+				<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+<hr/>
+
+<span class="accordian_title">In re Capacitors Antitrust Litigation (N.D. Cal.)</span><!-- accordian_title -->
+				
+				<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+				
 			</div>
 	
 			<h3>Data Breach</h3>
 			
 			<div>
 				
-				<span>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</span>
+				<span class="img_title"><img class="accordian_img" src="<?php bloginfo('template_directory');?>/images/results_antitrust.png"/>antitrust case summaries</span><!-- img_title -->
+				
+				<span class="accordian_title">In re Capacitors Antitrust Litigation (N.D. Cal.)</span><!-- accordian_title -->
+				
+				<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			
 			</div>
 	
@@ -35,6 +52,11 @@
 			
 			<div>
 				
+				<span class="img_title"><img class="accordian_img" src="<?php bloginfo('template_directory');?>/images/results_antitrust.png"/>antitrust case summaries</span><!-- img_title -->
+				
+				<span class="accordian_title">In re Capacitors Antitrust Litigation (N.D. Cal.)</span><!-- accordian_title -->
+				
+				<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 							
 			</div>
 			
@@ -43,6 +65,11 @@
 			
 			<div>
 				
+				<span class="img_title"><img class="accordian_img" src="<?php bloginfo('template_directory');?>/images/results_antitrust.png"/>antitrust case summaries</span><!-- img_title -->
+				
+				<span class="accordian_title">In re Capacitors Antitrust Litigation (N.D. Cal.)</span><!-- accordian_title -->
+				
+				<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 							
 			</div>
 			
@@ -51,6 +78,11 @@
 			
 			<div>
 				
+				<span class="img_title"><img class="accordian_img" src="<?php bloginfo('template_directory');?>/images/results_antitrust.png"/>antitrust case summaries</span><!-- img_title -->
+				
+				<span class="accordian_title">In re Capacitors Antitrust Litigation (N.D. Cal.)</span><!-- accordian_title -->
+				
+				<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 							
 			</div>
 			
@@ -59,6 +91,11 @@
 			
 			<div>
 				
+				<span class="img_title"><img class="accordian_img" src="<?php bloginfo('template_directory');?>/images/results_antitrust.png"/>antitrust case summaries</span><!-- img_title -->
+				
+				<span class="accordian_title">In re Capacitors Antitrust Litigation (N.D. Cal.)</span><!-- accordian_title -->
+				
+				<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 							
 			</div>
 
