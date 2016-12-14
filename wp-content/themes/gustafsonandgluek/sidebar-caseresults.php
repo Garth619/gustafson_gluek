60182dc11a10f1c9f043787077dba34265739697
Garrett Cullen <garrettwcullen@gmail.com>
2016-12-14 15:46:55 -0800

    push

--- wp-content/themes/gustafsonandgluek/sidebar-caseresults.php
+++ wp-content/themes/gustafsonandgluek/sidebar-caseresults.php
@@ -3,7 +3,7 @@
 	
 	<div class="inner_header_wrapper">
 	
-		<h1 class="inner_header"><?php the_title(); ?></h1><!-- inner_header -->
+		<h1 class="inner_header">Case Suggestions</h1><!-- inner_header -->
 	
 	</div><!-- inner_header_wrapper -->
 	
